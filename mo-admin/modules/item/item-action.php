52dd0d32dfb04690d2d8f3304e74b60c221b4799
Ricardo Garcia Rodriguez <master.ojitos@gmail.com>
2014-01-15 08:03:08 -0500

    terminado galeria de imagenes que hacemos en el admin.

--- mo-admin/modules/item/item-action.php
+++ mo-admin/modules/item/item-action.php
@@ -1,10 +1,15 @@
 <?php
-require_once "../conf.php";
+require_once '../conf.php';
+require_once '../class/MO_Image.class.php';
+$image_path = '../userfiles/';
 switch ($do) {
     case 5:
         $name = mo_scape($_POST['name']);
         $extra1 = mo_scape($_POST['extra1']);
         $thumbnail = mo_scape($_POST["imagec_guid"]);
+        $MO_Image = new MO_Image($image_path . $thumbnail);
+        $MO_Image->thumbnail(100, 100, true, false);
+        $MO_Image->save();
         $image = mo_scape($_POST["imageg_guid"]);
         $desc = mo_scape($_POST['desc']);
         $content = mo_scape($_POST['content']);
@@ -15,10 +20,13 @@ switch ($do) {
         $name = mo_scape($_POST['name']);
         $extra1 = mo_scape($_POST['extra1']);
         $thumbnail = mo_scape($_POST["imagec_guid"]);
+        $MO_Image = new MO_Image($image_path . $thumbnail);
+        $MO_Image->thumbnail(100, 100, true, false);
+        $MO_Image->save();
         $image = mo_scape($_POST["imageg_guid"]);
         $desc = mo_scape($_POST['desc']);
         $content = mo_scape($_POST['content']);
-        $cn->query("insert into image(id,category_id,name,thumbnail,image,description,content,extra1) VALUES ('',$select,'$name','$thumbnail','$image','$desc','$content','$extra1')");
+        $cn->query("insert into image(id,category_id,name,thumbnail,image,description,content,extra1,status) VALUES ('',$select,'$name','$thumbnail','$image','$desc','$content','$extra1','1')");
         break;
     case 4:
         $cn->query("DELETE from image WHERE id = " . $_POST['id']);
