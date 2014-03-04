a15d46194655618f02231014e9ce539c3b9d9ce2
Ricardo Garcia <master.ojitos@gmail.com>
2014-03-03 21:31:56 -0500

    reparando errores de versionb de php

--- conf.php
+++ conf.php
@@ -1,10 +1,14 @@
 <?php
 $masoji_dir = rtrim(realpath(dirname(__FILE__)), DIRECTORY_SEPARATOR);
-if($manager = opendir("$masoji_dir/class/")){
-	while(($file = readdir($manager)) !== false) 
-		if($file != "." and $file != "..") 
-			if(strtolower(end(explode(".", $file))) == "php") require_once "$masoji_dir/class/$file";
-	closedir($manager);
+if ($manager = opendir("$masoji_dir/class/")) {
+    while (($file = readdir($manager)) !== false) {
+        if ($file != "." and $file != "..") {
+            $file_parts = explode(".", $file);
+            if (strtolower(end($file_parts)) == "php")
+                require_once "$masoji_dir/class/$file";
+        }
+    }
+    closedir($manager);
 }
 $cn = Connection::getInstance();
 ?>
